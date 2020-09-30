6a17daa1151e182f435a2503f36362bf0afbc1b8
vinimoraess <vinicius_jag@hotmail.com>
2020-09-30 13:04:47 -0300

    Exercícios de interpretação finalizados

---
+++ Semana3/aula11/script.js
@@ -0,0 +1,29 @@
+/*---------EXERCÍCIOS INTERPRETAÇÃO
+
+EXERCÍCIO 1
+
+É criada uma variável que solicita ao usuário um número, porém como está no prompt esse numero será criado como string.
+
+Logo após valor digitado pelo usuário é convertido para o tipo Number e jogamos para a variável numero.
+No if é feita uma comparação se o resto da divisão do numero digitado pelo usuário por 2 for igual a 0 (tanto em tipo de variavel quanto valor), mostra no console a mensagem Passou no teste. Caso contrário mostra a mensagem Não passou no teste que está dentro do else.
+
+Resumindo, faz um teste para ver se o número é par.
+
+---------------------------------------------------------------------------------------
+
+EXERCÍCIO 2
+a. O código serve para o usuário inserir o nome de uma fruta e dependendo da fruta inserida o código insere um valor diferente na variável preco e mostra no final uma mensagem com o nome inserido e o preco dela.
+
+b. O preço da fruta Maçã é R$ 2.25
+
+c. Sem o brake o switch continua seu fluxo e adiciona o valor 5 para o preco. 
+-----------------------------------------------------------------------------------------
+
+EXERCÍCIO 3
+a. Na primeira linha está criando uma variável numero que recebe o que o usuário digitar e convertendo para o tipo Number.
+
+b. Se o usuário digitar 10 vai aparecer a mensagem Esse número passou no teste e um erro. Se ele digitar -10 não faz nada, pois não temos a condição else nesse caso, porém mostra o erro.
+
+c. Acontece um erro, pois a variável mensagem foi declarada no escopo do bloco if se tornando uma variável local.
+------------------------------------------------------------------------------------------
+*/
