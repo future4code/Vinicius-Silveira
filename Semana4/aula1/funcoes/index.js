cda94813dbeae20c0774c8708073e23894977ea1
vinimoraess <vinicius_jag@hotmail.com>
2020-10-05 12:34:30 -0300

    Exercícios de interpretação finalizados

---
+++ Semana4/aula1/funcoes/index.js
@@ -0,0 +1,19 @@
+/**-------------------EXERCÍCIOS DE INTERPRETAÇÃO 
+ * EXERCÍCIO 1
+ * a. Será impresso 10 e depois 50.
+ * b. No console não apareceria nada.
+ * --------------------------------------------------------------------------------------------
+ * 
+ * EXERCÍCIO 2
+ * a. É impresso cada os valores do índice 0 e 1, Darvas e Caio.
+ * b. Vai ser impresso os valores Amanda e Caio.
+ * --------------------------------------------------------------------------------------------
+ * 
+ * EXERCÍCIO 3
+ * Essa função faz uma potência de 2(eleva ao quadrado) dos números pares dentro do array.
+ * Poderia ser chamada de potenciaNumero
+ * --------------------------------------------------------------------------------------------
+ * 
+ * ---------------------EXERCÍCIOS DE ESCRITA
+ * 
+*/
