9c2f21d9aa3b69d8baf6b9ea38bc6e1f031d4e49
vinimoraess <vinicius_jag@hotmail.com>
2020-10-09 16:55:32 -0300

    Primeiro exercício de funções finalizado

---
+++ Semana4/lista-de-exercicios/funcoes.js
@@ -0,0 +1,23 @@
+/*const array = [23,45,10,20,50,90,77,88,93,4,75,78,91]
+let arrayMaior = []
+let arrayMenor = []
+let maior = -Infinity
+let menor = Infinity
+const segundoMaiorMenor = (array)=>{
+    for(let i=0;i<array.length;i++){
+        if (array[i]>maior){
+            arrayMaior.push(array[i])
+            maior=array[i]
+        }
+        if(array[i]<menor){
+            arrayMenor.push(array[i])
+            menor=array[i]
+        }
+    }
+    console.log(array)
+    console.log(maior)
+    console.log(menor)
+    console.log(arrayMenor)
+    console.log(arrayMaior)
+}
+segundoMaiorMenor(array)*/
