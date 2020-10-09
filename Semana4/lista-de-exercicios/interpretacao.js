64002aec584002062c9ca4e8145efcafd57b30df
vinimoraess <vinicius_jag@hotmail.com>
2020-10-09 13:01:22 -0300

    Segundo exercicio de interpretação finalizado

--- Semana4/lista-de-exercicios/interpretacao.js
+++ Semana4/lista-de-exercicios/interpretacao.js
@@ -7,7 +7,19 @@
  * como parâmetro da função. E no final é mostrado no console o valor convertido em reais
  * ---------------------------------------------------------------------------------------
  * 
- 
+ * EXERCÍCIO 2
+ * É criada uma função chamada investeDinheiro com 2 parâmetros, dentro dela é criada
+ * uma variável valorAposInvestimento que vai receber um valor de acordo com o tipo de
+ * investimento atravéz de um switch case, para cada opção a variável valorAposInvestimento
+ * recebe o valor passado na função multiplicado por um operador, 1.03 para Poupança,
+ * 1.05 para Renda Fixa, 1.06 para CDB e 1.1 para Ações, caso nenhum dos tipos seja informado
+ * um alert é mostrado na página com a mensagem "TIPO DE INVESTIMENTO INFORMADO INCORRETO!"
+ * e a função tem como retorno o valor calculado.
+ * Fora da função são criadas outras 2 variáveis que chamam a função e passam para ela na
+ * primeira variável os dados "Ações" e o valor 150 na segunda "Tesouro Direto" e valor 200.
+ * É mandado imprimir no console cada variável sendo que para novoMontante o valor retornado é 
+ * de 165 e na segunda é retornado o alert com a mensagem "TIPO DE INVESTIMENTO INFORMADO 
+ * INCORRETO!"
  */
 
 
