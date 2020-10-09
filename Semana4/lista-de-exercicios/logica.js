0ec46855e3227adafb81386562c8af9d6a0611b5
vinimoraess <vinicius_jag@hotmail.com>
2020-10-09 14:11:45 -0300

    Primeiro exercício de lógica finalizado

---
+++ Semana4/lista-de-exercicios/logica.js
@@ -0,0 +1,19 @@
+/**---------------------LÓGICA
+ * 
+ * EXERCÍCIO 1
+ * Podemos percorrer um array, usando, for, while,forEach, for of
+ * 
+ * const lista = [11,12,13,14,15,16,17,18,19,20]
+ *  for(let i=0; i<lista.length;i++){
+ *      console.log(lista[i])
+ * }
+ * let i=0
+ * while(i<lista.length){
+ *      console.log(lista[i])
+ *      i++
+ * }
+ * lista.forEach((numeros)=>{
+ *      console.log(numeros)
+ * })
+ * ------------------------------------------------------------------------------------------- 
+ */
