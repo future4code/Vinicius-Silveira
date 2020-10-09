c152e9212f2772c4f8996e77b539f9a3f7306a69
vinimoraess <vinicius_jag@hotmail.com>
2020-10-09 13:00:21 -0300

    Primeiro exercicio de interpretação finalizado

---
+++ Semana4/lista-de-exercicios/interpretacao.js
@@ -0,0 +1,14 @@
+/**--------------INTERPRETAÇÃO
+ * EXERCÍCIO 1
+ * É criada uma função chamada conversorDeMoeda com o parâmetro valorEmDolar
+ * dentro desta função é criada uma variável cotacao, a qual solicita ao usuário
+ * que digite o valor da cotacao do dólar e converte para número.
+ * O retorno dessa função é R$ + o valor da cotação multiplicado pelo valor passado
+ * como parâmetro da função. E no final é mostrado no console o valor convertido em reais
+ * ---------------------------------------------------------------------------------------
+ * 
+ 
+ */
+
+
+ 
