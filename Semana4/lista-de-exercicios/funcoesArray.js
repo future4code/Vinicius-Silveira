ac0062361cfe3fd5c1a12dc6da4d55f2533abbd3
vinimoraess <vinicius_jag@hotmail.com>
2020-10-10 15:19:08 -0300

    Segundo exercício de funções de array finalizado

--- Semana4/lista-de-exercicios/funcoesArray.js
+++ Semana4/lista-de-exercicios/funcoesArray.js
@@ -1,27 +1,50 @@
 /**----------------------------FUNÇÕES DE ARRAY
  * EXERCÍCIO 1
  * 
- */
-
-let pessoas=[
+ let pessoas=[
     {nome:'Pedro',idade:20},
     {nome:'Paulo',idade:10},
     {nome:'Paula',idade:12},
     {nome:'Artur',idade:89}
 ]
-console.log(pessoas)
-let retornaAdultos = pessoas.filter((dados)=>{
+a) let retornaAdultos = pessoas.filter((dados)=>{
     if(dados.idade>=20){
         return true
     }
     return false
 })
+console.log("Adultos: ",retornaAdultos)
+b)
 let retornaMenores = pessoas.filter((dados)=>{
     if(dados.idade<20){
         return true
     }
     return false
 })
-
-console.log("Adultos: ",retornaAdultos)
 console.log("Menores: ",retornaMenores)
+-----------------------------------------------------------------------------------------------
+
+EXERCÍCIO 2
+
+const array =[1,2,3,4,5,6]
+a)
+let multiplicaPorDois = array.map((dados)=>{
+    return dados*2
+})
+console.log(multiplicaPorDois)
+b)
+let multiplicaPorTres = array.map((dados)=>{
+    return `${dados*3}`
+})
+console.log(multiplicaPorTres)
+c)
+let parImpar = array.map((dados)=>{
+    if (dados%2===0){
+        return `${dados} é par`
+    }else {
+        return `${dados} é ímpar`
+    }
+})
+console.log(parImpar)
+-----------------------------------------------------------------------------------------------
+*/
