6af35f8049bc4fc1b31175c968bc05def27d0043
vinimoraess <vinicius_jag@hotmail.com>
2020-10-10 15:34:42 -0300

    Terceiro exercício de funções de array finalizado

--- Semana4/lista-de-exercicios/funcoesArray.js
+++ Semana4/lista-de-exercicios/funcoesArray.js
@@ -1,6 +1,6 @@
 /**----------------------------FUNÇÕES DE ARRAY
  * EXERCÍCIO 1
- * 
+ *  //Filter cria um novo array com o mesmo ou menor tamanho que o original
  let pessoas=[
     {nome:'Pedro',idade:20},
     {nome:'Paulo',idade:10},
@@ -25,7 +25,7 @@ console.log("Menores: ",retornaMenores)
 -----------------------------------------------------------------------------------------------
 
 EXERCÍCIO 2
-
+// MAP cria um novo array com o mesmo tamanho do original
 const array =[1,2,3,4,5,6]
 a)
 let multiplicaPorDois = array.map((dados)=>{
@@ -46,5 +46,34 @@ let parImpar = array.map((dados)=>{
     }
 })
 console.log(parImpar)
+-----------------------------------------------------------------------------------------------
+
+EXERCÍCIO 3
+const pessoas = [
+	{ nome: "Paula", idade: 12, altura: 1.8},
+	{ nome: "João", idade: 20, altura: 1.3},
+	{ nome: "Pedro", idade: 15, altura: 1.9},
+	{ nome: "Luciano", idade: 22, altura: 1.8},
+	{ nome: "Artur", idade: 10, altura: 1.2},
+	{ nome: "Soter", idade: 70, altura: 1.9}
+]
+//a)
+let permissaoEntrar = pessoas.filter((dados)=>{
+    if(dados.idade>14 && dados.idade<60 && dados.altura>=1.5){
+        return true
+    }                        
+    return false
+})
+//b)
+let naoEntra = pessoas.filter((dados)=>{
+    if(dados.idade<=14 || dados.idade>=60 || dados.altura<1.5){
+        return true
+    }                        
+    return false
+})
+console.log("===Podem usar a montanha Russa===")
+console.log(permissaoEntrar)
+console.log("===Não podem usar a montanha Russa===")
+console.log(naoEntra)
 -----------------------------------------------------------------------------------------------
 */
