bc9944bc0715d4ec012aace5d714ae4cd639a00b
Vinicius Silveira Moraes <vinicius_jag@hotmail.com>
2020-10-19 16:26:51 -0300

    Tarefas sem os desafios finalizadas

--- Semana6/aula1/labenu-forms/src/App.js
+++ Semana6/aula1/labenu-forms/src/App.js
@@ -4,17 +4,55 @@ import InfoSuperior from "./Components/Etapa2";
 import InfoGeraisEnsino from "./Components/Etapa3";
 import Final from "./Components/Final";
 
-import './App.css';
+//Transforma App em componente de classe
+class App extends React.Component{
+  state = {
+    estadoEtapa: 1
+  };
+  
+  renderizaEtapa = () =>{
+    // não é necessário o uso do break, pois com o return ela interrompe o switch
+    switch (this.state.estadoEtapa){
+      case 1:
+        return <DadosGerais/>
+      case 2:
+        return <InfoSuperior/>
+      case 3:
+        return <InfoGeraisEnsino/>
+      case 4:
+        return <Final/>
+      default:
+        return <DadosGerais/>        
+    }
+  }
 
-function App() {
-  return (
-    <div>
-      <DadosGerais/>    
-      <InfoSuperior/>
-      <InfoGeraisEnsino/>
-      <Final/>
-    </div>
-  );
+  avancaEtapa = ()=> {
+    /*if(this.state.estadoEtapa<4){
+      this.setState({
+        estadoEtapa: this.state.estadoEtapa+1
+      })
+    } */
+    const resultado = this.state.estadoEtapa<4 ? 
+      this.setState({estadoEtapa: this.state.estadoEtapa+1}) :
+      this.setState({estadoEtapa: this.state.estadoEtapa})
+    return resultado;    
+  }
+  voltaEtapa = ()=>{
+    const resultado = this.state.estadoEtapa>1 ?
+      this.setState({estadoEtapa:this.state.estadoEtapa-1}) :
+      this.setState({estadoEtapa:this.state.estadoEtapa})
+      return resultado
+  }
+  render(){
+    return(
+      <div>
+        {this.renderizaEtapa()}
+        <br/>
+        <button onClick={this.voltaEtapa}>Voltar Etapa</button>
+        <button onClick={this.avancaEtapa} >Próxima Etapa</button>
+
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
