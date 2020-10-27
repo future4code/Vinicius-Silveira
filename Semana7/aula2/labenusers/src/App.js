98d52238e44bd9b0e663a45afbf4bf527bfb629a
Vinicius Silveira Moraes <vinicius_jag@hotmail.com>
2020-10-27 16:54:14 -0300

    Finalizada a visualização dos usuários, faltando delete

--- Semana7/aula2/labenusers/src/App.js
+++ Semana7/aula2/labenusers/src/App.js
@@ -6,15 +6,16 @@ import ListaUsuarios from './components/ListaUsuarios/ListaUsuarios'
 
 class App extends React.Component{
   state={
-    telaCadastro:true,
-    usuarios:[],
+    telaCadastro:true,    
     inputUsuario:'',
     inputEmail:''
   }
+  
   //---------Função controlada que seta o estado do componente em false
   trocarComponente = ()=>{
     this.setState({telaCadastro:false})
   }
+
   //---------Função controlada que seta o estado do componente em true
   voltarComponente = ()=>{
     this.setState({telaCadastro:true})
@@ -23,14 +24,18 @@ class App extends React.Component{
   onChangeInputUsuario = (event)=>{
     this.setState({inputUsuario:event.target.value})
   }
+
   onChangeInputEmail = (event)=>{
     this.setState({inputEmail:event.target.value})
   }
+  
   criarUsuario = ()=>{
+    // pega os valores das inputs e joga no body que será usado para criar o usuario
     const body = {
       name: this.state.inputUsuario,
       email: this.state.inputEmail
     }
+    // cria o usuario na API
     axios.post('https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users',body,{
       headers:{
         Authorization:'vinicius-moraes-dumont'
@@ -42,7 +47,9 @@ class App extends React.Component{
       alert('Erro - Verifique se os campos estão corretos !')
     })      
   }
-  render(){
+
+  
+  render(){    
     //-------Função que verica se o estado está em true ou false e chama o componente ao clicar nos botões
     const trocaTela = ()=>{
       if(this.state.telaCadastro){
@@ -52,15 +59,18 @@ class App extends React.Component{
           onChangeInputUsuario={this.onChangeInputUsuario}
           onChangeInputEmail={this.onChangeInputEmail}
           onClickLista={this.trocarComponente} 
-          criarUsuario={this.criarUsuario}
+          criarUsuario={this.criarUsuario}          
         />
       }else{
-        return <ListaUsuarios onClickLogin={this.voltarComponente}/>
+        return <ListaUsuarios 
+        onClickLogin={this.voltarComponente}               
+        />
       }
     }
+
     return(
       <div>
-        {trocaTela()}
+        {trocaTela()}        
       </div>
     );
   }
--- Semana7/aula2/labenusers/src/components/ListaUsuarios/ListaUsuarios.js
+++ Semana7/aula2/labenusers/src/components/ListaUsuarios/ListaUsuarios.js
@@ -1,12 +1,40 @@
 import React from 'react'
+import axios from 'axios'
 
 class ListaUsuarios extends React.Component{
+    state={
+        usuarios:[]
+    }
+
+    componentDidMount = ()=>{
+        this.pegarUsuario();
+    }
+
+    pegarUsuario = ()=>{    
+        axios.get('https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users',{
+          headers:{
+            Authorization:'vinicius-moraes-dumont'
+          }
+        }).then((resposta)=>{
+          this.setState({usuarios:resposta.data});
+        }).catch((erro)=>{
+          console.log(erro.message);
+        })
+      }
+
     render(){
+        const atualizaLista = this.state.usuarios.map((dados)=>{
+            return(
+                <p key={dados.id}>{dados.name}</p>)
+            })
+
         return(
             <div>
                 <button onClick={this.props.onClickLogin}>Ir para página de cadastro</button>
                 <div>                    
-                    <h1>Usuários Cadastrados</h1>                 
+                    <h1>Usuários Cadastrados</h1>       
+                    {atualizaLista}  
+                    <button>X</button>        
                 </div>
             </div>
         )
