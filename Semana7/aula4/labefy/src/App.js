ab4defe4e817f9675ea968902d3624297e8f56be
Vinicius Silveira Moraes <vinicius_jag@hotmail.com>
2020-10-30 16:59:33 -0300

    Criado projeto labefy

---
+++ Semana7/aula4/labefy/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>APP</h1>
+      </div>
+    )
+  }
+}
+  
+export default App 
---
+++ Semana7/aula4/labefy/src/components/VisualizarPlayList/VisualizarPlayList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
+
+class VisualizarPlayList extends React.Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default VisualizarPlayList
