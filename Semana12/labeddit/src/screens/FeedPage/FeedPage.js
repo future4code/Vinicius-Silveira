6e43dd683637414882c2ed3e33f071e7cec16f04
Vinicius Silveira Moraes <vinicius_jag@hotmail.com>
2020-12-05 03:54:41 -0300

    Finalizado Criação de post

--- Semana12/labeddit/src/screens/FeedPage/FeedPage.js
+++ Semana12/labeddit/src/screens/FeedPage/FeedPage.js
@@ -6,11 +6,15 @@ import axios from 'axios'
 import {baseUrl} from '../../constants/baseUrl'
 import {headers} from '../../constants/headers'
 import { ContainerCard } from './Styled'
+import { useForm } from '../../hooks/useForm'
 
 function FeedPage (){    
     const [posts,setPosts]=useState([])
     const [load,setLoad]=useState(false)
-
+    const {form,onChange,resetState}=useForm({
+        postTitle:'',
+        postText:''
+    })
     useProtectedPage()
 
     const renderPosts = ()=>{
@@ -41,10 +45,52 @@ function FeedPage (){
             alert('Erro na votação, tente novamente !')
         })        
     }
+
+    const onChangeInput=(event)=>{
+        const{name,value}=event.target
+        onChange(name,value)
+    }
+    const submitPost = (event)=>{
+        event.preventDefault()
+        const body={
+            text:form.postText,
+            title:form.postTitle            
+        }
+        axios.post(`${baseUrl}/posts`,body,headers)
+        .then(()=>{
+            alert('Post criado com sucesso !')
+            renderPosts()
+        })
+        .catch(()=>{
+            alert('Não foi possível criar o post, revise os dados')
+        })
+        resetState()
+    }
     return(
         <ContainerCard>
             <Header/>
             {load && <progress/>}
+            <form onSubmit={submitPost}>
+                <input
+                    id='postTitle'
+                    name='postTitle'
+                    placeholder='Título do Post'
+                    value={form.postTitle}
+                    title='Informe um título'
+                    onChange={onChangeInput}
+                    required
+                />
+                <input
+                    id='postText'
+                    name='postText'
+                    placeholder='Texto do Post'
+                    value={form.postText}
+                    title='Informe o conteúdo do Post'
+                    onChange={onChangeInput}
+                    required
+                />
+                <button>Criar Post</button>
+            </form>
             {posts && posts.map((post)=>{
                 return(
                     <PostCard
