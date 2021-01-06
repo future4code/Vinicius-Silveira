6035fc86b0f5de3d4480ece631470c3332f46039
Vinicius Silveira Moraes <vinicius_jag@hotmail.com>
2021-01-06 18:00:57 -0300

    Endpoints finalizados

--- Semana15/aula42/src/index.ts
+++ Semana15/aula42/src/index.ts
@@ -1,6 +1,6 @@
 import express,{Request,Response} from "express"
 import cors from "cors"
-import {countries} from "./countries"
+import {countries,country} from "./countries"
 import {AddressInfo} from "net"
 const app = express()
 
@@ -16,7 +16,7 @@ app.get('/countries/all', (req:Request,res:Response)=>{
 })
 //endpoint 3
 app.get('/countries/search',(req:Request, res:Response)=>{
-    const filterCountries = countries.filter(
+    /*const filterCountries = countries.filter(
         country=> country.name.includes(req.query.name as string)
     )
     const filterCapital = countries.filter(
@@ -25,12 +25,36 @@ app.get('/countries/search',(req:Request, res:Response)=>{
     const filterContinent = countries.filter(
         continent=>continent.continent.includes(req.query.continent as string)
     )
-    res.status(200).send(filterContinent)
+    res.status(200).send(filterContinent)*/
+    let result: country[] = countries
+    if(req.query.name || req.query.capital || req.query.continent){
+        if (req.query.name) {
+            result = result.filter(
+                country => country.name.includes(req.query.name as string)
+            )
+        }
+    
+        if (req.query.capital) {
+            result = result.filter(
+                country => country.capital.includes(req.query.capital as string)
+            )
+        }
+    
+        if (req.query.continent) {
+            result = result.filter(
+                country => country.continent.includes(req.query.continent as string)
+            )
+        }
+        res.status(200).send(result)
+    }else{
+        res.status(404).send('Nenhum parâmetro de busca foi informado !')
+    }
+    
 })
 
 //endpoint 2
 app.get('/countries/:id', (req:Request,res:Response)=>{
-    const result = countries.find(
+    const result : country | undefined = countries.find(
         country=>country.id===Number(req.params.id))
     if(result){
         res.status(200).send(result)
@@ -39,7 +63,13 @@ app.get('/countries/:id', (req:Request,res:Response)=>{
     }    
 })
 
+//endpoint 4
+app.put('/countries/edit/:id', (req:Request,res:Response)=>{
+    countries[Number(req.params.id)].name=req.body.name
+    countries[Number(req.params.id)].capital=req.body.capital
+    res.status(200).send(req.body)
 
+})
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
        const address = server.address() as AddressInfo;
