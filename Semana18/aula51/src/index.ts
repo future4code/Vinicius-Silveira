530268dc5c3f3748dc80418dd97b39d955e2fa2c
Vinicius Silveira Moraes <vinicius_jag@hotmail.com>
2021-01-26 16:56:31 -0300

    enpoint para buscar usuários finalizado

--- Semana18/aula51/src/index.ts
+++ Semana18/aula51/src/index.ts
@@ -5,6 +5,7 @@ import { signup } from "./endpoints/signup"
 import { login } from "./endpoints/login"
 import { selectUserById } from "./endpoints/selectUserById"
 import { removeUserById } from "./endpoints/removeUserById"
+import { selectUserByIdNoPermission } from "./endpoints/selectUserByIdNoPermission"
 
 const app = express()
 app.use(express.json())
@@ -12,8 +13,10 @@ app.use(cors())
 
 app.post("/signup",signup)
 app.post("/login",login)
+app.get("/user",selectUserByIdNoPermission)
 app.get("/user/profile",selectUserById)
 app.delete("/user/:id",removeUserById)
+
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
       const address = server.address() as AddressInfo;
